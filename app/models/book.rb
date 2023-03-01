096b7f8bf6b7d80eae49196e65ec3df49fc1840f
EC2 Default User <ec2-user@ip-172-31-11-206.ap-northeast-1.compute.internal>
2023-03-01 04:52:17 +0000

    [meddle]task2-3

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
 
   has_one_attached :profile_image
   belongs_to :user
+  has_many :favorites,dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
@@ -13,5 +14,10 @@ class Book < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
      end
      profile_image.variant(resize_to_limit: [width, height]).processed
+    end
+
+   def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
    end
+
 end
